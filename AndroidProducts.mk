0c1f1c395f71b6aba6e1e322edfa6268c09b9dd3
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-08-22 01:28:57 -0700

    Initialize eleven

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_mojito.mk
+    
+COMMON_LUNCH_CHOICES := \
+    aosp_mojito-eng \
+    aosp_mojito-userdebug \
+    aosp_mojito-user
