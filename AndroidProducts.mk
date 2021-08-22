24c24b97793f8fa4563a3578b1ea927d911703d0
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-08-22 01:29:02 -0700

    Initialize 11-nad-vanilla

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_mojito.mk
+    
+COMMON_LUNCH_CHOICES := \
+    nad_mojito-eng \
+    nad_mojito-userdebug \
+    nad_mojito-user
