36fd78a4dca3351d531c951fa1e816e55323a63a
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-08-22 01:28:55 -0700

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/radiant_mojito.mk
+    
+COMMON_LUNCH_CHOICES := \
+    radiant_mojito-eng \
+    radiant_mojito-userdebug \
+    radiant_mojito-user
